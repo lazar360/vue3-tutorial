eea5cb042f1bd9e1bba162e9956eb3e2d72de1c8
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-02-11 21:48:56 +0100

    first method in vueJs

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div>hello {{person.name}}</div>
+    <div>My age is {{person.age}}</div>
+    <div v-if="person.isDev">I am a dev</div>
+    <div v-else>I am not a dev</div>
+    <button @click="increaseMyAgeAndChangeMyName()">Increase my age</button>
+  </div>
+</template>
+<script>
+export default{
+data(){
+  return{
+    person :{
+      name:'Nicolas ',
+      age:43,
+      isDev : false
+    }
+  }
+},
+methods:{
+  increaseMyAgeAndChangeMyName(){
+    this.person.name = 'Kevin'
+    this.person.age++
+  }
+}
+}
+</script>
