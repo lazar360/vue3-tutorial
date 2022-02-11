b8cac06260561a9f2c0266fa812266bc4bf3fa90
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-02-11 22:36:07 +0100

    add view todo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
 <template>
   <div>
-    <div>hello {{person.name}}</div>
-    <div>My age is {{person.age}}</div>
-    <div v-if="person.isDev">I am a dev</div>
-    <div v-else>I am not a dev</div>
-    <button @click="increaseMyAgeAndChangeMyName()">Increase my age</button>
+    <div>Add a todo :</div>
+    <input type="text">
   </div>
 </template>
 <script>
 export default{
 data(){
   return{
-    person :{
-      name:'Nicolas ',
-      age:43,
-      isDev : false
+     
     }
-  }
-},
-methods:{
-  increaseMyAgeAndChangeMyName(){
-    this.person.name = 'Kevin'
-    this.person.age++
-  }
-}
+  },
 }
 </script>
