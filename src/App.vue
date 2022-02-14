45cbbbc5a973067c310efc77c4ad2029ff7350c5
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-02-14 20:47:37 +0100

    TodoList

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,31 @@
 <template>
   <div>
     <div>Add a todo :</div>
-    <input type="text">
+    <input type="text" v-model="value">
+    <button @click="addToTodo">Add to todolist</button>
+        <div>My todolist :</div>
+        <ul>
+          <li v-for="(item, itemIndex) in toDolist" v-bind:key="itemIndex">
+             {{item}}
+          </li>
+        </ul>
+
+
   </div>
 </template>
 <script>
 export default{
 data(){
   return{
-     
+    toDolist : ['Go to the haircutter', 'buy some coffee', 'Go to work'],
+     value: undefined
     }
   },
+  methods : { 
+      addToTodo() {
+      this.toDolist.push(this.value)
+      this.value=undefined
+    }
+  }
 }
 </script>
